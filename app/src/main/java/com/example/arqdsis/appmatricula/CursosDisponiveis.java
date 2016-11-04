6e58c961bb5e213f0cc71a7591478e5c45640a9b
Rodrigo Baldan <rodrigo.baldan16@hotmail.com>
2016-11-04 10:12:22 -0200

    SistemaMatricula

---
+++ app/src/main/java/com/example/arqdsis/appmatricula/CursosDisponiveis.java
@@ -0,0 +1,13 @@
+package com.example.arqdsis.appmatricula;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CursosDisponiveis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cursos_disponiveis);
+    }
+}
---
+++ app/src/main/java/com/example/arqdsis/appmatricula/TelaMatricula.java
@@ -0,0 +1,13 @@
+package com.example.arqdsis.appmatricula;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaMatricula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_matricula);
+    }
+}
